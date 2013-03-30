e613c2b24d10059dbbde6025cd9c56dc2302f5a1
Dmitry Vorotilin <d.vorotilin@gmail.com>
2013-03-30 14:55:21 +0400

    Added support for other editors

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # Show me the method
 
 This tiny gem can pretty easy open the method you need in your favorite text
-editor(Textmate only for now, PRs are welcome). I spend a lot of time in rails
-console and I often have to see how some methods look like and now I can
-do it easily:
+editor. I found myself spending a lot of time in rails console and I often have
+to see how some methods look like and with this gem I can do it easily:
 
     User.show_method(:find_by_sql)
 
-or even
+or even with short alias `s`:
 
     ActiveRecord::ConnectionAdapters::Column.s :string_to_time
 
--- lib/show_method.rb
+++ lib/show_method.rb
@@ -1,8 +1,34 @@
-class Object
+module ShowMethod
+  class Error < StandardError; end
+
+  def self.command
+    editor = ENV['EDITOR'] || 'vi'
+    command = case editor
+              when 'mvim', 'vim', 'vi', 'emacs', 'nano'
+                '%{editor} +%{line} %{file}'
+              when 'mate'
+                '%{editor} -l %{line} %{file}'
+              when 'subl'
+                '%{editor} %{file}:%{line}'
+              end
+    raise Error, "Editor #{editor} isn't supported" unless command
+    [command, editor]
+  end
+
+  def self.method_location(object, name)
+    file, line = object.method(name).source_location
+    raise Error, "Method `#{name}` is written on C language" unless file
+    [file, line]
+  end
+
   def show_method(name)
-    editor = ENV['EDITOR']
-    file, line = method(name).source_location
-    `#{editor} -l #{line} #{file}`
+    file, line = ShowMethod.method_location(self, name)
+    command, editor = ShowMethod.command
+    system(command % { :editor => editor, :line => line, :file => file })
+  rescue ShowMethod::Error => error
+    warn error.message
   end
   alias :s :show_method
 end
+
+Object.send :include, ShowMethod
